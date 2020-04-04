d53f756f67af07972ed39b2b980364808ccdae5b
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-04-04 16:03:55 +0500

    issue resolve

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // rout file for user e.g login, signup, 
-const { signUpFN, } = require("@controllers/user/SignUpController"),
+const { signUpFN, } = require("@controllers/user/signUpController"),
   { loginFN, } = require("@controllers/user/LoginController"),
   { updateUserProfile } = require("@controllers/user/userController");
 
